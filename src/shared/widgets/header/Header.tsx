0c2bf7e61a595679962a3e84f9f99701c5362b76
RahulB <rahulbisht1012@gmail.com>
2024-03-06 00:32:17 +0530

    Branding Component Completed
    created a component for our partner brands
    and added new logos in the public/assets folder
    make changes in the constands as well.

--- src/shared/widgets/header/Header.tsx
+++ src/shared/widgets/header/Header.tsx
@@ -7,7 +7,7 @@ import Toolbar from "./Toolbar";
 const Header = () => {
     return (
         <>
-            <header className="w-full sticky top-0 left-0 z-[999] px-10 pt-2 flex items-center justify-between h-[60px] font-Lexend shadow-md border-b border-b-gray-800">
+            <header className="w-full sticky top-0 left-0 z-[999] px-10 pt-2 flex items-center justify-between h-[60px] font-Lexend shadow-md border-b border-b-gray-800 backdrop-blur-sm">
                 <div className="flex items-center justify-between">
                     <Link href={"/"}>
                         <Logo />
