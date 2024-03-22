c0dd394f99e163f833790866ed75472d7b35fe6c
RahulB <rahulbisht1012@gmail.com>
2024-03-23 00:03:13 +0530

    Homepage updates

--- src/modules/home/elements/Benefits.tsx
+++ src/modules/home/elements/Benefits.tsx
@@ -4,13 +4,13 @@ const Benefits = () => {
     return (
         <>
             <div className="flex min-h-[60vh] items-center justify-center mt-2">
-                <div className="w-[95%] sm:w-[70%] bg-transparent text-[#ffffff] border rounded-2xl border-blue-500 p-6 sm:p-10 sm:px-6">
-                    <h3 className="font-Lexend font-[800] uppercase text-2xl md:text-5xl text-center w-full mb-4">
-                        EVERYTHING YOU NEED TO <span className="bg-[#FFC83E] font-Lexend text-black">SUCCEED</span>{" "}
+                <div className="w-[95%] sm:w-[70%] bg-transparent border rounded-2xl border-green-500 p-6 sm:p-10 sm:px-6">
+                    <h3 className="font-Lexend font-[900] uppercase text-2xl md:text-5xl text-center w-full mb-4">
+                        EVERYTHING YOU NEED TO <span className="bg-green-500 font-Lexend text-white">SUCCEED</span>{" "}
                         AVAILABLE IN A SINGLE PLATFORM
                     </h3>
 
-                    <p className="text-md md:text-xl text-center font-Lexend">
+                    <p className="text-sm md:text-lg text-center font-Lexend">
                         Add in your own website. No complex integrations.
                     </p>
                 </div>
