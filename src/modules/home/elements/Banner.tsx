7377b5ecfaf5a6b49145dfbdc1c46d2e5c7481e5
RahulB <rahulbisht1012@gmail.com>
2024-03-22 23:27:47 +0530

    Header and Banner changes

--- src/modules/home/elements/Banner.tsx
+++ src/modules/home/elements/Banner.tsx
@@ -5,22 +5,24 @@ const Banner = () => {
         <section className="bg-[#f7f9fa] text-black font-Lexend">
             <div className="mx-auto max-w-screen-xl px-2 py-14 lg:flex lg:items-center">
                 <div className="mx-auto max-w-3xl text-center">
-                    <h1 className="text-4xl bg-gradient-to-r from-green-500 via-blue-500 to-purple-600 bg-clip-text  text-transparent font-extrabold  sm:text-7xl mb-2">
-                        Create Emails
-                    </h1>
-                    <span className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text font-extrabold text-transparent  sm:block font-Lexend text-3xl sm:text-5xl">
-                        That Your Audience Loves
+                    <div className="inline-flex gap-1">
+                        <h1 className="text-4xl font-extrabold sm:text-7xl mb-2">Create </h1>
+                        <h1 className="text-4xl font-extrabold sm:text-7xl mb-2 text-green-500"> Emails</h1>
+                    </div>
+
+                    <span className="font-extrabold sm:block font-Lexend text-3xl sm:text-6xl">
+                        That Your Audience💖
                     </span>
 
-                    <p className="mx-auto mt-4 px-5 max-w-xl sm:text-md relaxed font-Lexend text-black ">
+                    <p className="mx-auto mt-4 px-5 text-lg text-green-500">
                         Digital marketing tools to grow your audience faster and drive revenue smarter.
                     </p>
 
                     <div className="mt-8 flex flex-wrap justify-center gap-5">
-                        <Button className="w-[90%] border tracking-wider border-[#7856ff] bg-[#7856ff] px-12 py-3 text-sm rounded-lg hover:text-black focus:outline-none focus:ring active:text-opacity-75 sm:w-auto h-12">
+                        <Button className="w-[90%] border tracking-wider bg-green-500 px-12 py-3 text-sm rounded-lg text-black sm:w-auto h-12">
                             Get Started
                         </Button>
-                        <Button className="w-[90%] rounded-lg tracking-wider border border-[#7856ff] px-12 py-3 text-sm  bg-transparent h-12 sm:w-auto">
+                        <Button className="w-[90%] rounded-lg tracking-wider border border-green-500 px-12 py-3 text-sm text-green-500  bg-transparent h-12 sm:w-auto">
                             Go To Docs
                         </Button>
 
